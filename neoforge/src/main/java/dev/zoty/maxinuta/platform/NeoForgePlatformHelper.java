dec17696aacf4337f48b4ff86f2b7ff3787a62b6
Zoty <leonardokronbauer2002@gmail.com>
2024-05-04 07:51:23 -0300

    Update to dsl

---
+++ neoforge/src/main/java/dev/zoty/maxinuta/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package dev.zoty.maxinuta.platform;
+
+import dev.zoty.maxinuta.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
